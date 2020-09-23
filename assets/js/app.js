4dcb27f53462751befa49d8852fc1c5fa88b47bc
Autonomousse <ucsddatacamp@gmail.com>
2020-09-22 19:54:24 -0700

    Added a few more functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,10 +29,11 @@ var chartGroup = svg.append("g")
 var chosenXAxis = "poverty";
 var chosenYAxis = "healthcare";
 
-// Function used to update x-scale variable when the user clicks on the axis label
+// Function used to update the x-scale variable when the user clicks on the axis label
 function xScale(riskData, chosenXAxis) {
 
   // Use scaleLinear to create the scale since values are numerical
+  // Set the min and max values so we can transition the scale later
   var xLinearScale = d3.scaleLinear()
     .domain([d3.min(riskData, values => values[chosenXAxis]) /* some number to further pad*/, // CAN THIS BE CHANGED TO d3.extent? Check with console.log
     d3.max(riskData, values => values[chosenXAxis]) /* some number to further pad*/
@@ -40,10 +41,51 @@ function xScale(riskData, chosenXAxis) {
     .range([0, width]);
 
   // Return the scale
-  return xLinearScale
+  return xLinearScale;
 }
 
+// Function used to update the y-scale variable when the user clicks on the axis label
 function yScale(riskData, chosenYAxis) {
 
   // Use scaleLinear to create the scale since values are numerical
+  // Set the min and max values so we can transition the scale later
+  // height first because the chart is created top down (0,0 is at the top left)
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(riskData, values => values[chosenYAxis]) /* some number to further pad*/, 
+    d3.max(riskData, values => values[chosenYAxis]) /* some number to further pad*/
+  ])
+    .range([height, 0]);
+
+  // Return the scale
+  return yLinearScale;
+}
+
+// Function used to update the x-axis variable when the user clicks the axis label
+function renderXAxis(newLinearXScale, xAxis) {
+
+  // Call the axisBottom function to update the axis values
+  var bottomAxis = d3.axisBottom(newLinearXScale);
+
+  // Transisition to the newly chosen x-axis
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+
+  // Return the new x-axis
+  return xAxis;
+}
+
+// Function used to update the y-axis variable when the user clicks the axis label
+function renderYAxis(newLinearYScale, yAxis) {
+
+  // Call the axisLeft function to update the axis values
+  var leftAxis = d3.axisLeft(newLinearYScale);
+
+  // Transition to the newly chosen y-axis
+  yAxis.transition()
+  .duration(1000)
+  .call(leftAxis);
+
+  // Return the new y-axis
+  return yAxis;
 }
